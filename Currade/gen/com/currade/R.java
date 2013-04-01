7226a493ae97132d1d563fce415fe6446e8da2f4
stephendiadamo <stephen.diadamo@gmail.com>
2013-03-31 22:16:01 -0400

    SD - Changed icon

--- Currade/gen/com/currade/R.java
+++ Currade/gen/com/currade/R.java
@@ -22,7 +22,9 @@ public final class R {
     }
     public static final class drawable {
         public static final int hat=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int hat_back=0x7f020001;
+        public static final int hat_whiteout=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
         public static final int CourseNameLabel=0x7f080007;
